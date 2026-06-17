0dcbdbaac6989238fbc562c96c361bb854f18fe8
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-17 21:41:16 +0530

    terraform execution role added to code

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -1,5 +1,10 @@
 module "s3_backend" {
   source = "./modules/s3_backend"
   bucket_name = "tf-state-030179310796"
+}
 
+module "terraform-role" {
+  source = "./modules/terraform-role"
+  user_arn = "arn:aws:iam::030179310796:user/ramarao"
+  role_name = "Terraform-execution-role"
 }
