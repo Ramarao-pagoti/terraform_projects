4e022e4b2c2305e4dcace39e4d5561ca5a8c3155
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-19 01:24:47 +0530

    modified VPC output file as incorrect outputs detected

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -5,15 +5,15 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   description = "The IDs of the public subnets"
-  value       = values(aws_subnet.public[*].id)
+  value       = values(aws_subnet.public[*])
 }
 
 output "private_app_subnet_ids" {
   description = "The IDs of the private application subnets"
-  value       = values(aws_subnet.private_app[*].id)
+  value       = values(aws_subnet.private_app[*])
 }
 
 output "private_db_subnet_ids" {
   description = "The IDs of the private database subnets"
-  value       = values(aws_subnet.private_db[*].id)
+  value       = values(aws_subnet.private_db[*])
 }
