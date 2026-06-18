a27eb1a9e93b6b905e86605c72fa24b334d48fe4
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-18 20:35:18 +0530

    backend configured for state file _ dev environment

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tf-state-030179310796"
-    key = "/dev/networking/terraform.tfstate"
+    key = "dev/networking/terraform.tfstate"
     region = "us-west-2"
     use_lockfile = true
   }
