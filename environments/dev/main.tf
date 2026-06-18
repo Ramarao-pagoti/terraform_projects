8825773a82e94f68abeb2a4b46e2f7833837f2e6
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-18 19:39:23 +0530

    wrong vpc cidr mentioned

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source = "../../modules/VPC"
   environment = "dev"
-    vpc_cidr = "10.0.0.16"
+    vpc_cidr = "10.0.0.0/16"
     availability_zones = ["us-west-2a", "us-west-2b"]
 }
