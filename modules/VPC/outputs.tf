ceb829c0335c9467594aa543af1e2fccf93a6d9a
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-19 00:10:18 +0530

    removed some outputs in VPC module and later will add

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -1,34 +1,4 @@
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.this.id
-  }
-
-output "dev-public-a"   {
-  description = "Public Subnet in AZ A"
-  value       = aws_subnet.public["us-west-2a"].id
-}
-
-output "dev-public-b" {
-  description = "Public Subnet in AZ B"
-  value       = aws_subnet.public["us-west-2b"].id
-}
-
-output "dev-private-app-a" {
-  description = "Private App Subnet in AZ A"
-  value       = aws_subnet.private_app["us-west-2a"].id
-}
-
-output "dev-private-app-b" {
-  description = "Private App Subnet in AZ B"
-  value       = aws_subnet.private_app["us-west-2b"].id
-}
-
-output "dev-private-db-a" {
-  description = "Private DB Subnet in AZ A"
-  value       = aws_subnet.private_db["us-west-2a"].id
-}
-
-output "dev-private-db-b" {
-  description = "Private DB Subnet in AZ B"
-  value       = aws_subnet.private_db["us-west-2b"].id
 }
