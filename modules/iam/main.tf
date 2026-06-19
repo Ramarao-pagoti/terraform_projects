514c28bacc9923f8c8131a4b66dab650d4cfa3bd
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-20 01:22:37 +0530

    IAM module modified for OIDC implementation

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -119,5 +119,5 @@ resource "aws_iam_policy" "alb_controller" {
 
 resource "aws_iam_role_policy_attachment" "alb_controller" {
     policy_arn = aws_iam_policy.alb_controller.arn
-    role = aws_iam_policy.alb_controller.name
+    role = aws_iam_role.alb_controller.name
 }
