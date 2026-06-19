fcbc04aebc8758f8b1ae80cd8a9e9923b5c66dc1
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-19 20:11:23 +0530

    EKS Module modified

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -15,3 +15,22 @@ resource "aws_eks_cluster" "this" {
     "scheduler"
   ]
 }
+
+resource "aws_eks_node_group" "system" {
+    cluster_name = aws_eks_cluster.this.name
+    node_group_name = "$var.environment}-system-ng"
+    node_role_arn = var.node_role_arn
+    subnet_ids = var.private_subnet_ids
+    capacity_type = "ON_DEMAND"
+    ami_type = "AL2023_x86_64_STANDARD"
+    instance_types = ["t3.medium"]
+    scaling_config {
+      desired_size = 2
+      min_size = 2
+      max_size = 5
+    }
+    update_config {
+      max_unavailable = 1
+    }
+    depends_on = [ aws_eks_cluster.this ]
+}
