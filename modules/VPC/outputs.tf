2f2c49f31a7b1ce9dedd40d4f732dca327e0e42e
Ramarao_pagoti <pagotiramarao@gmail.com>
2026-06-19 17:55:48 +0530

    IAM module json document modified

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -5,3 +5,10 @@ output "vpc_id" {
 output "private_app_subnet_ids" {
     value = values(aws_subnet.private_app)[*].id
 }
+output "public_subnet_ids" {
+    value = values(aws_subnet.public)[*].id  
+}
+output "private_data_subnet_ids" {
+    value = values(aws_subnet.private_data)[*].id
+  
+}
